5e1bc781a35ab74cd7cd60568eb7bfd95daeb07d
shaurya5 <gargshaurya28@gmail.com>
2023-09-30 04:31:48 +0530

    card frontend changes

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,37 @@
 import React from "react";
 
 function Card({ name, address, distance, lat, long }) {
-  const googleMapsLink = `https://www.google.com/maps/place/${lat},${long}`
+  const googleMapsLink = `https://www.google.com/maps/place/${lat},${long}`;
   return (
     <div className="w-[20rem] h-[18rem] rounded overflow-hidden shadow-lg">
       <div className="px-6 py-4">
         <div className="font-bold text-xl mb-2">{name}</div>
         <p className="text-gray-700 text-base">
-          <strong>Address: </strong>{address}
+          <strong>Address: </strong>
+          {address}
         </p>
         <p className="text-gray-700 text-base">
-          <strong>Distance: </strong>{distance / 1000} km
+          <strong>Distance: </strong>
+          {distance / 1000} km
         </p>
-        <a href={googleMapsLink} className="text-blue-700">Google Maps Link</a>
+        <a href={googleMapsLink} className="text-blue-700 flex flex-row items-center justify-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+            fill="#000000"
+            version="1.1"
+            id="Capa_1"
+            width="20px"
+            height="20px"
+            viewBox="0 0 395.71 395.71"
+            xmlSpace="preserve"
+          >
+            <g>
+              <path d="M197.849,0C122.131,0,60.531,61.609,60.531,137.329c0,72.887,124.591,243.177,129.896,250.388l4.951,6.738   c0.579,0.792,1.501,1.255,2.471,1.255c0.985,0,1.901-0.463,2.486-1.255l4.948-6.738c5.308-7.211,129.896-177.501,129.896-250.388   C335.179,61.609,273.569,0,197.849,0z M197.849,88.138c27.13,0,49.191,22.062,49.191,49.191c0,27.115-22.062,49.191-49.191,49.191   c-27.114,0-49.191-22.076-49.191-49.191C148.658,110.2,170.734,88.138,197.849,88.138z" />
+            </g>
+          </svg>
+          Google Maps Link
+        </a>
       </div>
     </div>
   );
